035eb815a3d928cda4aab62063a6bbe993da3197
Shruti More <moreshruti382@gmail.com>
2026-07-24 21:24:38 +0530

    downgrade Spring Boot to 3.5.5 and add JWT dependencies

--- src/main/java/com/saas/springbackend/SaasSpringBackendApplication.java
+++ src/main/java/com/saas/springbackend/SaasSpringBackendApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SaasSpringBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SaasSpringBackendApplication.class, args);
+		System.out.println("Application running..");
 	}
 }
